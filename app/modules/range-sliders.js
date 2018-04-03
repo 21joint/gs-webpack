569d8826b0aa56a956f536118ff5d9c31f0bff8e
SAMVELHARUTYUNYAN <samvelharutyunyan777@gmail.com>
2018-04-04 02:12:58 +0400

    updates

--- app/modules/range-sliders.js
+++ app/modules/range-sliders.js
@@ -1,52 +1,56 @@
 import noUiSlider from 'nouislider';
 import _ from 'lodash';
 
-export default function() {
-  let App;
+export default function () {
+    let App;
 
-  App = function() {
-    let _self = this;
+    App = function () {
+        let _self = this;
 
-    _self._instances = [];
+        _self._instances = [];
 
-    _self.init = function(selector, options) {
-      let sliders = document.querySelectorAll(selector);
+        _self.init = function (selector, options) {
+            let sliders = document.querySelectorAll(selector);
 
-      _.each(sliders, function(el, i) {
-        let _options,
-            _slider,
-            _start,
-            _min,
-            _max;
+            _.each(sliders, function (el, i) {
+                let _options,
+                    _slider,
+                    _start,
+                    _min,
+                    _max,
+                    _snap;
 
 
-        _start = el.dataset.start.split(',');
-        _min = parseInt(el.dataset.min);
-        _max = parseInt(el.dataset.max);
+                _start = el.dataset.start.split(',');
+                _min = parseInt(el.dataset.min);
+                _max = parseInt(el.dataset.max);
+                _snap = el.dataset.snap;
+                console.log(snap);
 
-        _.each(_start, function(s, i) {
-          _start[i] = Number(_start[i]) ?
-              _start[i] :
-              eval('_' + _start[i].slice(1, _start[i].length - 1));
-        });
+                _.each(_start, function (s, i) {
+                    _start[i] = Number(_start[i]) ?
+                        _start[i] :
+                        eval('_' + _start[i].slice(1, _start[i].length - 1));
+                });
 
-        _options = _.extend({}, options, {
-          connect: true,
-          start: _start,
-          range: {
-            'min': _min,
-            'max': _max,
-          },
-        });
+                _options = _.extend({}, options, {
+                    connect: true,
+                    start: _start,
+                    range: {
+                        'min': _min,
+                        'max': _max,
+                    },
+                    snap: _snap
+                });
 
-        _slider = noUiSlider.create(el, _options);
-       console.log(el, _slider);
-        _self._instances.push(_slider);
-      });
-      console.log(_self._instances);
-      return _self;
+                _slider = noUiSlider.create(el, _options);
+                console.log(el, _slider);
+                _self._instances.push(_slider);
+            });
+            console.log(_self._instances);
+            return _self;
+        };
     };
-  };
 
-  return new App();
+    return new App();
 }
